e71e3c662b0b573e73eb20c7b2b943810b48bd9f
fhaddad0 <ferashaddad2000@gmail.com>
2025-08-30 23:46:39 +0100

    Added Spring dependencies

---
+++ src/main/java/com/musicspace/MusicSpace/MusicSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.musicspace.MusicSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicspace/MusicSpace/MusicSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicspace.MusicSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
